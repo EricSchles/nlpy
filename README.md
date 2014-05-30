1bb62ca5874b11052cb00eed5da174e6962d56ef
EricSchles <ericschles@gmail.com>
2014-05-30 04:36:47 -0700

    ngrams done

---
+++ README.md
@@ -0,0 +1,47 @@
+#nlpy -  A natural language processing library written in python
+
+How to use the modules:
+
+import nlpy
+
+nlpy.ngram:
+
+A simple Ngram analyzer.
+
+Methods:
+
+ngrams(n): n is an integer.
+
+Returns the ngram for the initialized string.
+
+uni_gram(): 
+
+Returns the unigram for a given initialized string.
+
+bi_gram():
+
+Returns the bigram for a given initialized string.
+
+tri_gram():
+
+Returns the trigram for a given initialized string.
+
+comparison(other_ngram_object):
+
+Takes in an Ngram object and compares it against the initialized Ngram object.
+
+
+##Examples:
+
+from nlpy import ngram.Ngram
+
+x = Ngram("Hello there sally")
+y = Ngram("How are you sally?")
+
+x.comparison(y)
+
+x.ngrams(5)
+
+x.uni_gram()
+x.bi_gram()
+x.tri_gram()
---
+++ ngram.py
@@ -0,0 +1,36 @@
+class Ngram:
+    def __init__(self, sentence):
+        self.sentence = sentence.split(' ')
+        self.unigram = self.uni_gram()
+        self.bigram = self.bi_gram()
+        self.trigram = self.tri_gram()
+
+    def ngrams(self,n):
+        n_gram = []
+        for ind,word in enumerate(self.sentence):
+            n_gram.append(self.sentence[ind:ind+n])
+        return n_gram
+
+    def uni_gram(self):
+        return self.ngrams(1)
+    
+    def bi_gram(self):
+        return self.ngrams(2)
+
+    def tri_gram(self):
+        return self.ngrams(3)
+
+    #assumes other is also an Ngram object
+    def comparison(self,other):
+        unigram_count = len([gram for gram in self.unigram if gram in other.unigram])
+        bigram_count = len([gram for gram in self.bigram if gram in other.bigram])
+        trigram_count = len([gram for gram in self.trigram if gram in other.trigram])
+
+        word_choice_sim = float(unigram_count) / len(self.unigram) 
+        word_choice_sim *= 100 
+        ave_choice_sim = ((float(bigram_count) / len(self.bigram)) + (float(trigram_count) / len(self.trigram))) / 2
+        ave_choice_sim *= 100
+        report = ["This writing sample is about %s%% in terms of word choice" % (str(word_choice_sim)),
+                  "This writing sample uses about %s%% of the same phrasing as the provided second sample" % (str(ave_choice_sim))
+                  ]
+        return report
